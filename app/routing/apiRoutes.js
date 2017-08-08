9c4deb810c60cf0f23ca68691f21cf89d866e58c
Myles Allen <mylesvallen@icloud.com>
2017-08-07 21:47:06 -0700

    files

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,7 @@
 //API routes for data 
-//data see and data the user can post to the server
-var friendFinderData = require('/data/friends.js');
+//data see and data the user can post to the server 
+var friendFinderData = require('../data/friends.js');
+var path = require('path');
 
 module.exports = function(app) {
     app.get('/api/friends', function(require, response) {
